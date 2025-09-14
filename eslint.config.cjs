768cbf52f12f6e710d1efc642bb380ae28f6f633
Senait <senaigetachew@gmail.com>
2025-09-14 15:08:43 -0600

    chore(lint): add ESLint v9 flat config and fix lint script

---
+++ eslint.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('eslint').Linter.FlatConfig[]} */
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+    },
+    rules: {
+      'no-console': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      '.github/workflows/**',
+      'package-lock.json',
+    ],
+  },
+];
