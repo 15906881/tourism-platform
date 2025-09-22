ce13c14c9e619a240736133c061d64f1318671ba
CRAN2025 <senaigetachew@gmail.com>
2025-09-22 01:37:51 -0600

    Update server.js

--- api/server.js
+++ api/server.js
@@ -12,7 +12,6 @@ const dbUrl =
 // Use connection string SSL parameters with proper SSL handling
 const pool = new Pool({ 
   connectionString: dbUrl,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
   ssl: { rejectUnauthorized: false }
 });
 
