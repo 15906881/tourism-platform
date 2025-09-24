a25a56ea9f6e76de730615c3ed9b2dd1bb16a8fc
Senait <senaigetachew@gmail.com>
2025-09-24 13:09:26 -0600

    chore(lint): wire lint/format; add web/package.json; pin ESLint v8

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,7 +1,12 @@
 const js = require("@eslint/js");
 module.exports = {
   env: { browser: true, es2023: true },
-  extends: [js.configs.recommended, "plugin:react/recommended", "plugin:react-hooks/recommended", "prettier"],
+  extends: [
+    js.configs.recommended,
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "detect" } }
+  settings: { react: { version: "detect" } },
 };
