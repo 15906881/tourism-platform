23eacc0b19a010ad644c6a88772038cb512e7bd2
Senait <senaigetachew@gmail.com>
2025-10-19 08:03:52 -0600

    fix(ui): remove references to non-existent AppShell components

--- packages/ui/src/components/AppShell/index.ts
+++ packages/ui/src/components/AppShell/index.ts
@@ -1,17 +1,3 @@
 export { AppShell } from './AppShell';
-export type { AppShellProps } from './AppShell';
-
-export { Sidebar, SidebarItem } from './Sidebar';
-export type { SidebarProps, SidebarItemProps } from './Sidebar';
-
-export { PageHeader } from './PageHeader';
-export type { PageHeaderProps } from './PageHeader';
-
-export { ContentArea } from './ContentArea';
-export type { ContentAreaProps } from './ContentArea';
-
-export { EmptyState } from './EmptyState';
-export type { EmptyStateProps } from './EmptyState';
-
-export { Skeleton, SkeletonText } from './Skeleton';
-export type { SkeletonProps } from './Skeleton';
+export { Skeleton } from './Skeleton';
+// Header, Footer, Navigation components don't exist - removed from exports
